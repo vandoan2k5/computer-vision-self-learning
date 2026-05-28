952866390c4e51a941290fe0111d7f3ceaf7c440
root <mynameisdoan05@gmail.com>
2026-05-28 16:58:22 +0000

    BUOC DAU TIEN: LAY DATA CIFAR100

---
+++ get_data.py
@@ -0,0 +1,10 @@
+from huggingface_hub import snapshot_download
+
+local_dir = "./data"
+
+snapshot_download(
+    repo_id="doannvptit/cv_cifar100", 
+    repo_type="dataset", 
+    local_dir=local_dir,
+    local_dir_use_symlinks=False  # Quan trọng: False để tải file thực tế thay vì link ảo
+)
